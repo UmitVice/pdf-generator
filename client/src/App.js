4a45390ef9888f03eb64885148f874df55f38060
Ümithan Viçe <umitvice@gmail.com>
2021-12-03 23:25:40 +0300

    First setup

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
+
+import './App.css';
+
+function App() {
+  state = {
+    name: '',
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+  }
+
+  return (
+    <div className="App">
+      <input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
+      <input type="Receipt ID" placeholder="receiptId" name="name" onChange={this.handleChange} />
+      <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
+      <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
+      <button onClick={this.createAndDownloadPdf}>Download PDF</button>
+    </div>
+  );
+}
+
+export default App;
